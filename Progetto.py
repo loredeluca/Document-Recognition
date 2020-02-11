75066bd67c5d75ddad16045cf482148f9ec04445
manuelsap96 <60352049+manuelsap96@users.noreply.github.com>
2020-02-11 15:38:29 +0100

    merged mine and your code

--- Progetto.py
+++ Progetto.py
@@ -1,11 +1,11 @@
 import cv2 as cv
 import numpy as np
 import math
+import statistics
 from matplotlib import pyplot as plt
 from skimage.filters import (threshold_otsu, threshold_sauvola)
 from skimage.transform import hough_line
 from skimage import measure
-from pythonRLSA import rlsa
 import sklearn.preprocessing 
 from scipy.sparse.csgraph import minimum_spanning_tree
 from sklearn.neighbors import kneighbors_graph
@@ -39,6 +39,9 @@ def binarization(mode,image):
         return sklearn.preprocessing.binarize(image, threshold_otsu(image))
     elif mode == 'sauvola':
         return sklearn.preprocessing.binarize(image, threshold_sauvola(image))
+    elif mode == 'inverse':
+        _,thresh = cv.threshold(image,127,255,cv.THRESH_BINARY_INV)
+        return thresh
         
 
 def showImage(text_name, file_name):
@@ -56,29 +59,36 @@ def showImage(text_name, file_name):
     cv.waitKey(0)
     cv.destroyWindow(text_name)
 
-def removeFiguresAndSpots(binarized_img,original_img):
+def removeFiguresOrSpots(binarized_img,original_img, mode):
     '''
-    Remove figures and spots from images, useful for making more accurate the deskew with Hough transform.
+    Remove figures or spots from images, useful for making more accurate the deskew with Hough transform.
     
     Parameters
     ----------
     binarized_img : array of binarized image pixels.
     original_img: array of original image pixels.
+    mode : string used for selection of mode. 
 
     Returns
     -------
-    img_no_figures : array of original image pixels without figures.
+    new_img : array of original image pixels without figures or spots.
     '''
     contours,_ = cv.findContours(np.uint8(np.logical_not(binarized_img)), cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
-    img_no_figures = original_img.copy()
+    new_img = original_img.copy()
     for contour in contours:
         [x,y,w,h] = cv.boundingRect(contour)
-        if w>300 or h>300 or (0<=w<=7) or (0<=h<=7):#remove if the box it's too big (figure) or if it's too small (spot)
-            for i in range(y,y+h):
-                for j in range(x, x+w):
-                    img_no_figures[i][j] = (255,255,255)
-    showImage('gn',img_no_figures)
-    return img_no_figures
+        if mode == 'figures':
+            if w>400 or h>300 :#remove if the box it's too big (figure) or if it's too small (spot)
+                for i in range(y,y+h):
+                    for j in range(x, x+w):
+                        new_img[i][j] = (255,255,255)
+        elif mode == 'spots':
+            if ((0<=w<=7) and (0<=h<=7)):
+                for i in range(y,y+h):
+                    for j in range(x, x+w):
+                        new_img[i][j] = (255,255,255)
+    #showImage('gn',new_img)
+    return new_img
         
 
 def printContours(binarized_img,output_img):
@@ -94,96 +104,109 @@ def printContours(binarized_img,output_img):
     contours,_  = cv.findContours(np.uint8(np.logical_not(binarized_img)),cv.RETR_EXTERNAL,cv.CHAIN_APPROX_SIMPLE) 
     for contour in contours:
         [x,y,w,h] = cv.boundingRect(contour)
-        if w > 4 and h > 4:
+        if w >= 4 and h >= 4:
             cv.rectangle(output_img, (x,y), (x+w,y+h), (0, 255, 0), 1)
  
-   
-
-
-
-def pixelDistance(image, vertical: bool = False):
-    if vertical:
-        image = image.T #la uso per calcolare la distanza tra pixel in verticale
+    
+def iteration(image, value: int):
     rows, cols = image.shape
-    distance=[]
-    count=0
-    i1=0
-    flag=0
-    #i2=0
+    for row in range(rows):
+        try:
+            start = image[row].tolist().index(0)
+        except ValueError:
+            start = 0
 
-    for i in range(rows):
-        for j in range(cols):
-            
-            if image[i][j]==255 and image[i][0]==255:
-                '''
-                if i!=i2:
-                    flag=1
-                    i2=i
-                else:
-                    flag=flag+1
-                    i2=i
-                    '''
-            if image[i][j]==0:
-                if i!=i1:
-                    distance.append('-1')
-                    i1=i
-                    count=0
-                elif j-count-flag!=0:
-                        distance.append(j-count-flag)
-                        flag=0
-                        count=j
-                    
-    distance=np.array(distance)
-    return distance
+        count = start
+        for col in range(start, cols):
+            if image[row, col] == 0:
+                if (col-count) <= value and (col-count) > 0:
+                    image[row, count:col] = 0               
+                count = col  
+    return image 
 
-def valueRLSA(distance):
-    rows=distance.shape[0]
+
+    #RLSA consiste nell'estrarre il blocco di testo o la Regione di interesse(ROI) dall'immagine binaria
+    #del documento. Bisogna passargli un'immagine binaria di tipo ndarray.
+def rlsa(image, horizontal: bool = True, vertical: bool = True, value: int = 0):
+    if horizontal:
+            image = iteration(image, value)
+    if vertical: 
+            image = image.T
+            image = iteration(image, value)
+            image = image.T
+    return image
+   
+def showCC(binarized_img,conn):
+    cc, labels = cv.connectedComponents(~binarized_img,connectivity=conn)
+    print('The Connected Components are ', cc)
+    
+    label_color = np.uint8(179*labels/cc-1)
+    blank_channel = 255*np.ones_like(label_color)#creates a white matrix
+    labeled_img = cv.merge([label_color, blank_channel, blank_channel])#creates an image in RGB
+    
+    labeled_img = cv.cvtColor(labeled_img, cv.COLOR_HSV2BGR)#convert to BGR for display
+
+    labeled_img[label_color==0] = 0 #set backgroud label to black
+    
+    return labeled_img
+
+
+def findMidDistanceContour(binarized_img):
+    contours,_  = cv.findContours(np.uint8(np.logical_not(binarized_img)),cv.RETR_EXTERNAL,cv.CHAIN_APPROX_SIMPLE) 
+    sumW = 0
+    size = 0
+    for contour in contours:
+        [x,y,w,h] = cv.boundingRect(contour)
+        sumW += w
+        size += 1
+    midW = sumW/size
+    return midW
+    
+
+def valueRLSA(binarized_img, vert: bool = False):
+    
+    distance = np.asarray(findDistance(binarized_img, vert))
     numSum=0
     sumDist=0 
     distance = distance.astype(np.int)
     
-    for k in range(rows):
-        if distance[k]!= -1:
-            sumDist=sumDist+distance[k]
-            numSum=numSum+1  
-            
+    for k in range(distance.shape[0]):
+        sumDist += distance[k]
+        numSum += 1  
+           
     value=sumDist/numSum
-    print(value)
-    return value
+    midW = findMidDistanceContour(binarized_img)
+    value -= midW
+    return round(value),distance
 
-def histogram(image,distance):
-    rows=distance.shape[0]
-    distance = distance.astype(np.int)
-    max=0
-    for h in range(rows):
-        if distance[h]>max:
-            max=distance[h]
+#trova la distanza tra centroidi
+def findDistance(binarized_img, vert):
+    points = findCentroids(binarized_img,binarized_img.copy())
+    G,edges = minimumSpanningTreeEdges(points,4)
+    edges_dist = G.data
+    edges_hor,edges_vert = edgesInformation(edges,points,edges_dist)
+    distance = []
+    edges = edges_hor
+    if vert:
+        edges = edges_vert
+    for edge in edges:
+        c1,c2 = edge[2]
+        x1,y1 = points[c1]
+        x2,y2 = points[c2]
+        dist = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+        distance.append(dist)
+    return distance
+
+
+def histogram(binarized_img,distance):
+    w=findMidDistanceContour(binarized_img)
+    row_number = [i for i in range(len(distance))]
+    distances=[]
+    for j in range(len(distance)):
+        distances.append(distance[j]-w)
+    plt.bar(row_number,distances)
+    plt.show()        
     
-    #serve per rendere il grafico piu leggibile, elimina pixel a distanza 1
-    q1=0
-    for l in range(rows):
-        if distance[l]==1 and q1<200:
-            q1=q1+1
-        elif distance[l]==1:
-            distance[l]=-1
-            
-    #plt.imshow(image,'gray'), plt.xticks([]), plt.yticks([]), plt.show()
-    plt.hist(distance,256,[0,30]), plt.show()
-    #plt.savefig('hist.png', dpi=1800)
-    
-
-'''
-pix_dist_horiz=pixelDistance(bin_sauvola*255)
-pix_dist_vert=pixelDistance(bin_sauvola*255,True)
-value_horiz=valueRLSA(pix_dist_horiz)
-value_vert=valueRLSA(pix_dist_vert)
-histogram(bin_sauvola,pix_dist_horiz)
-histogram(bin_sauvola,pix_dist_vert)
-'''
-
-
-
-
 
 
    
@@ -242,21 +265,14 @@ def houghTransformDeskew(binarized_img,original_img):
     return None
 
 
-def projection(img_bin, img_gray):
-    #Counting black pixels per row (axis=0: col, axis=1:row)
+def projection(img_bin):
+    
+    #Counting black pixels (img_bin==0) per row (axis=0: col, axis=1:row)
     counts = np.sum(img_bin==0, axis=1) 
     row_number = [i for i in range(img_bin.shape[0])]
     #counts = smooth(counts,20) #ammorbidisce il grafico dei pixel
+    return counts, row_number
     
-    f, (ax1, ax2) = plt.subplots(1, 2, sharey=True, figsize=(30, 15))
-    ax1.imshow(img_gray,'gray'), plt.yticks([])
-    ax1.tick_params(axis='both', which='major', labelsize=10)
-    ax2.plot(counts,row_number,label='fit')
-    ax2.tick_params(axis='both', which='major', labelsize=10)
-    plt.xlabel('Number of Black Pixels',fontsize=20)
-    plt.ylabel('Row Number',fontsize=20)
-    plt.subplots_adjust( wspace = -0.1)
-    plt.savefig('Projection.png')#, dpi=1800)
     
    
 
@@ -427,6 +443,7 @@ def minimumSpanningTreeEdges(points, k):
     k : int the number of neighbor to consider for each vector.
     
     Returns:
+    full_tree :
     edges : numpy array of connected nodes in the MST. Index couple of points.     
     '''
     G = kNeighborsGraph(points, k)
@@ -434,7 +451,7 @@ def minimumSpanningTreeEdges(points, k):
     # Compute the minimum spanning tree of this graph
     full_tree = minimum_spanning_tree(G, overwrite=True)
 
-    return np.array(full_tree.nonzero()).T 
+    return full_tree,np.array(full_tree.nonzero()).T 
 
 
 def angleBetween(p1, p2):
@@ -473,7 +490,34 @@ def docstrum(input_img, output_img, edges, points, thresh_dist):
             plt.plot([points[i, 0], points[j, 0]], [points[i, 1], points[j, 1]], c='r')
     plt.show()
 
+def edgesInformation(edges, points, distances):
+    angles = getAngles(edges,points)
+    
 
+    horizontal_edges =[]
+    vertical_edges = []
+    munnezza = []
+    for i in range(len(angles)):
+    
+        #if -24< angles[i] < 24 or 156 < angles[i] or angles[i] < -156:
+        if -10< angles[i] < 10 or 170 < angles[i] or angles[i] < -170:
+            horizontal_edges.append((angles[i],distances[i],[edges[i][0],edges[i][1]]))
+        elif 80 < angles[i] < 100 or (-80 > angles[i] and angles[i] > -100)  :
+            vertical_edges.append((angles[i],distances[i],[edges[i][0],edges[i][1]]))
+        else:
+            munnezza.append((angles[i],distances[i],[edges[i][0],edges[i][1]]))
+    '''
+    print('HORIZ')
+    for i in range(len(horizontal_edges)):
+        print(horizontal_edges[i])
+    print('VERT')
+    for i in range(len(vertical_edges)):
+        print(vertical_edges[i])
+    print('MUN')
+    for i in range(len(munnezza)):
+        print(munnezza[i])
+    '''
+    return horizontal_edges,vertical_edges
 
 def main():
     
@@ -486,40 +530,50 @@ def main():
     
     bin_otsu = binarization('otsu',img_gray)
     bin_sauvola = binarization('sauvola',img_gray)
+    bin_inv = binarization('inverse',img_gray)
     
         
     
     #showImage('Original Image',img)
+    #showImage('Otsu Binarization',bin_inv)
     #showImage('Otsu Binarization',bin_otsu*255)
     #showImage('Sauvola Binarization', bin_sauvola*255)
 
     #bin_sauvola = cv.medianBlur(bin_sauvola, 3) #apply median blur to remove black spots on images
-    showImage('Sauvola Binarization BLUR', bin_sauvola*255)
+    #showImage('Sauvola Binarization BLUR', bin_sauvola*255)
 
-    labels = measure.label(bin_sauvola, background = 1, connectivity=2)
-
-    plt.figure(figsize=(20,20))
-    plt.imshow(labels, 'nipy_spectral')
-    plt.title('Connected Components')
-    plt.axis('off')
-    plt.show()
+    
+    
+    
+    img_no_spots = removeFiguresOrSpots(bin_sauvola,img,'spots')
+    bin_no_spots = binarization('sauvola',img_no_spots)
+    
+    labeled_image = showCC(bin_no_spots*255,8)
+    
+    #showImage('cc',labeled_image)
+    
+    img = img_no_spots
+    bin_sauvola = bin_no_spots
     
     img_bin_char = bin_sauvola.copy()
     printContours(img_bin_char,img_bin_char)    
     #showImage('Characters Contour', img_bin_char*255)    
 
-    #DA VERIFICARE RLSA ADATTIVO, HO FATTO VARI TEST MA LA DIVISIONE DELLE PAROLE NON FUNZIONA BENE
-
-    #img_rlsa_oriz = rlsa.rlsa(img_bin_char.copy(), True, False, 10)  
-    #img_rlsa_full = rlsa.rlsa(img_rlsa_oriz.copy(), False, True, 3)
-
-    img_rlsa_full = rlsa.rlsa(bin_sauvola.copy(), True, True, 10)
+    valueH,dist = valueRLSA(bin_sauvola)
+    histogram(bin_sauvola,dist)
+    print(valueH)
+    valueV, dist = valueRLSA(bin_sauvola,True)
+    histogram(bin_sauvola,dist)
+    print(valueV)
+    
+    img_rlsa_H = rlsa(bin_sauvola.copy(), True, False, valueH)
+    img_rlsa_full = rlsa(img_rlsa_H.copy(),False,True,valueV)
 
     #showImage('RLSA',img_rlsa_full*255)
     printContours(img_rlsa_full,img_word)
-    #showImage('Words Contour', img_word)
+    showImage('Words Contour', img_word)
     
-    img_no_figures = removeFiguresAndSpots(img_rlsa_full,img)
+    img_no_figures = removeFiguresOrSpots(img_rlsa_full,img,'figures')
     #showImage('Image Without Figures', img_no_figures)
  
     img_rotated, img_rotated_no_figures = houghTransformDeskew(img_no_figures,img)
@@ -540,12 +594,16 @@ def main():
     
     points = findCentroids(bin_rotated,img_centroids)
     #showImage('Centroids',img_centroids)
-    voronoi(points, img_voro) 
+    #voronoi(points, img_voro) 
     
     Graph = kNeighborsGraph(points,5)           
     k_kneighbors_edges = np.array(Graph.nonzero()).T
     k_kneighbors_distances = Graph.data
-
+    
+    media_dist = statistics.mean(k_kneighbors_distances)
+    print('media',media_dist)
+    print('max',max(k_kneighbors_distances))
+    print('min',min(k_kneighbors_distances))
 
     row_number = [i for i in range(k_kneighbors_distances.shape[0])]
     plt.figure(figsize=(20,10))
@@ -553,7 +611,7 @@ def main():
     plt.show()
 
 
-    mst_edges= minimumSpanningTreeEdges(points,5)
+    _,mst_edges= minimumSpanningTreeEdges(points,5)
     
     
     points = np.int32(points)
@@ -562,7 +620,8 @@ def main():
     plotEdges(img,k_kneighbors_edges,points)
     plotEdges(img,mst_edges,points)
     
-    
+    horizontal_edges, vertical_edges = edgesInformation(k_kneighbors_edges, points, k_kneighbors_distances)
+    '''
     angles = getAngles(k_kneighbors_edges,points)
     
     horizontal_edges =[]
@@ -574,30 +633,30 @@ def main():
             horizontal_edges.append((angles[i],k_kneighbors_distances[i],[k_kneighbors_edges[i][0],k_kneighbors_edges[i][1]]))
         else:
             vertical_edges.append((angles[i],k_kneighbors_distances[i],[k_kneighbors_edges[i][0],k_kneighbors_edges[i][1]]))
-
+            '''
     
     #DOCSTRUM ORIZZONTALE
-    docstrum(img, img_k, horizontal_edges, points, 65)
+    docstrum(img, img_k, horizontal_edges, points, max(k_kneighbors_distances)/2)
     
     img_k_orig = img_k.copy()
 
     showImage('K-NN Lines',img_k) 
     bin_img_k = binarization('sauvola',img_k)
-    rlsa_docstrum = rlsa.rlsa(bin_img_k.copy(), True, False, 2)
-    showImage('K-NN Lines rlsa',rlsa_docstrum*255) 
+    rlsa_docstrum = rlsa(bin_img_k.copy(), True, False, 2)
+    #showImage('K-NN Lines rlsa',rlsa_docstrum*255) 
     printContours(rlsa_docstrum,img_docstrum_lines)
     showImage('Docstrum lines',img_docstrum_lines)
     
     
 
     #DOCSTRUM VERTICALE
-    docstrum(img, img_k, vertical_edges, points, 64)
+    docstrum(img, img_k, vertical_edges, points, max(k_kneighbors_distances)/2)
     
 
 
     showImage('K-NN Lines',img_k) 
     bin_img_k = binarization('sauvola',img_k)
-    rlsa_docstrum = rlsa.rlsa(bin_img_k.copy(), True, True, 1)
+    rlsa_docstrum = rlsa(bin_img_k.copy(), False, True, 1)
     printContours(rlsa_docstrum,img_docstrum_box)
     showImage('Docstrum lines',img_docstrum_box)
     
